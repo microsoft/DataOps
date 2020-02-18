f7ce4ed2fc0f9bd16fda87a94b5407ac902c9f28
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-02-18 22:02:17 +0100

    Update variables.tf

--- Python/azureml-pipeline-databricks/infrastructure/terraform/azureml/variables.tf
+++ Python/azureml-pipeline-databricks/infrastructure/terraform/azureml/variables.tf
@@ -23,12 +23,12 @@ variable "devops_mlpipeline_sp_object_id" {
   description = "Service principal object ID for the Azure DevOps ML Model CI/CD pipeline service connection"
 }
 
-variable "devops_mlpipeline_sp_client_id" {
+variable "aml_run_sp_client_id" {
   type    = string
   description = "Service principal client ID for the Azure DevOps ML Model CI/CD pipeline service connection"
 }
 
-variable "devops_mlpipeline_sp_client_secret" {
+variable "aml_run_sp_client_secret" {
   type    = string
   description = "Service principal client secret for the Azure DevOps ML Model CI/CD pipeline service connection"
 }
