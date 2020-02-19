949928b62e2cc3e5cf619a08b1e5176f6ee89305
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-02-19 07:24:43 +0100

    Update main.tf

--- Python/azureml-pipeline-databricks/infrastructure/terraform/azureml/main.tf
+++ Python/azureml-pipeline-databricks/infrastructure/terraform/azureml/main.tf
@@ -152,3 +152,10 @@ resource "azurerm_key_vault_secret" "client_secret" {
   key_vault_id = azurerm_key_vault.aml.id
   depends_on   = [azurerm_key_vault_access_policy.client_keyvault]
 }
+
+resource "azurerm_key_vault_secret" "tenant_id" {
+  name         = "TenantId"
+  value        = var.tenant_id
+  key_vault_id = azurerm_key_vault.aml.id
+  depends_on   = [azurerm_key_vault_access_policy.client_keyvault]
+}
