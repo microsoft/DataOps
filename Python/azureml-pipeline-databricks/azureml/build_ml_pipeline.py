240335ed21bb038923e8a43f1912751e2c8ad2f5
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-02-19 13:33:57 +0100

    .

--- Python/azureml-pipeline-databricks/azureml/build_ml_pipeline.py
+++ Python/azureml-pipeline-databricks/azureml/build_ml_pipeline.py
@@ -226,7 +226,7 @@ def main():
 
     training_dataprep_step = DatabricksStep(
         name="FeatureEngineering",
-     #   inputs=[training_data_input],
+        inputs=[training_data_input],
         outputs=[feature_eng_output],
         spark_version=DATABRICKS_RUNTIME_VERSION,
         instance_pool_id=instance_pool_id,
