d32c623553b8fb3718481f3642c0f2700dc947e0
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-02-25 18:14:52 +0100

    Update setup.py

--- Python/packages/databricks-test/setup.py
+++ Python/packages/databricks-test/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='databricks_test',
-    version='0.0.1',
+    version='0.0.2',
     author="Alexandre Gattiker",
     author_email="algattik@microsoft.com",
     description="Unit testing and mocking for Databricks",
